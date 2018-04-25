e75cdd9ce392a7da207fd08e4a9a2ef49014de8c
Jaime Rueda <jaime_rueda10@hotmail.com>
2018-04-24 21:42:15 -0500

    curso de devf, parte para usar JS, CSS y HTML juntos)

---
+++ html-js.js
@@ -0,0 +1,28 @@
+let h1 = document.createElement("h1");
+     let texto_h1 = document.createTextNode('eh que onda, soy un H1 con JS');
+     h1.appendChild(texto_h1);
+
+     let elbody = document.getElementById('elBody');
+     elbody.appendChild(h1);
+
+     let cinta_blacna = [
+    {
+        'nombre' : 'Omar',
+        'edad': 24 
+    },
+    {
+        'nombre': 'Francisco',
+        'edad': 39
+    },
+    {
+        'nombre' : 'carlos',
+        'edad' : 24
+    }
+
+]
+for(i=0; i<cinta_blacna.length; i++){
+    let h1 = document.createElement('h1');
+    let textos = document.createTextNode('este wei se llama'+cinta_blacna[i].nombre+'y tiene'+cinta_blacna[i].edad+'años');
+    h1.appendChild(textos);
+    elbody.appendChild(h1);
+}
---
+++ json.js
@@ -0,0 +1,28 @@
+let cinta_blacna = [
+    {
+        'nombre' : 'Omar',
+        'edad': 24 
+    },
+    {
+        'nombre': 'Francisco',
+        'edad': 39
+    },
+    {
+        'nombre' : 'carlos',
+        'edad' : 24
+    }
+
+]
+
+let sujeto={
+    'nombre': 'Israel',
+    'edad' : 25
+}
+
+console.log(suijeto);
+
+console.log('este wei se llama'+cinta_blacna[1].nombre+ 
+            'y tiene'+cinta_blacna[1].edad+'años');
+
+for(i=0;i<cinta_blacna.length;i++)
+console.log('este wei se llama'+cinta_blacna[i].nombre+' y tiene'+cinta_blacna[i].edad+' años');
